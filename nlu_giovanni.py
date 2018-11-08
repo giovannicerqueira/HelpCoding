6a14e2ad307cd5750362b45c631d96b0fb8967fe
giovannicerqueira <35223506+giovannicerqueira@users.noreply.github.com>
2018-11-08 18:04:35 -0300

    Update nlu_giovanni.py

--- nlu_giovanni.py
+++ nlu_giovanni.py
@@ -9,8 +9,8 @@ from watson_developer_cloud.natural_language_understanding_v1 \
   import Features, EntitiesOptions, KeywordsOptions, SentimentOptions, CategoriesOptions
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
-  username='31a0b567-7c67-4000-90a9-12ff29f56446',
-  password='OWY6mgLqVLSX',
+  username='xxxxxxxxxxxx',
+  password='xxxxxxxx',
   version='2018-03-16')
 
 #vr_text = sys.argv[1]
@@ -22,7 +22,7 @@ response = natural_language_understanding.analyze(
   features=Features(
     categories=CategoriesOptions(),
     entities=EntitiesOptions(
-        model='7216ab41-66b8-4fd2-bbc2-0741e0a12f4f')
+        model='xxxxxxxxxxxxx')
     ))
 
 #print("Palavras-chave encontradas:")
